41c7aeea7e1b2fd70d1b7f7418114ac8bfebe08c
thomasGR9 <117160330+thomasGR9@users.noreply.github.com>
2024-05-24 17:03:58 +0300

    add files for site

---
+++ script.js
@@ -0,0 +1,28 @@
+function getPrediction() {
+    const redCorner = document.getElementById('red-corner').value;
+    const blueCorner = document.getElementById('blue-corner').value;
+    const resultBox = document.getElementById('result-box');
+
+    if (!redCorner || !blueCorner) {
+        resultBox.textContent = 'Please enter names for both fighters.';
+        return;
+    }
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://ufc-winner-predictor-ee463ee67161.herokuapp.com', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                const response = JSON.parse(xhr.responseText);
+                resultBox.textContent = `Winner: ${response.result}`;
+            } else {
+                resultBox.textContent = 'Error predicting winner. Please try again.';
+            }
+        }
+    };
+
+    const data = JSON.stringify({ red_corner_name: redCorner, blue_corner_name: blueCorner });
+    xhr.send(data);
+}
